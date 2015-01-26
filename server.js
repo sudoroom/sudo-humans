4dc3fbb44b7df6ad6e18049dfc2ae70c9ae6a6d6
James Halliday <mail@substack.net>
2015-01-25 21:39:19 -0800

    layout

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 var hyperstream = require('hyperstream');
 var ecstatic = require('ecstatic')({
     root: __dirname + '/static',
     gzip: true
 });
-var persona = require('persona-id')('http://localhost:7000');
-
-var sessions = {};
-persona.on('create', function (sid, id) {
-    sessions[sid] = id.email;
-});
-
-persona.on('destroy', function (sid) {
-    delete sessions[sid];
-});
 
 var server = http.createServer(function (req, res) {
-    if (persona.test(req)) {
-        persona.handle(req, res);
+    if (req.url === '/') {
+        layout('index.html');
     }
-    else if (req.url === '/') {
-        var sid = persona.getId(req);
+    else ecstatic(req, res);
+    
+    function layout (page) {
         res.setHeader('content-type', 'text/html');
-        fs.createReadStream(__dirname + '/static/index.html')
-            .pipe(hyperstream({ '#name': sessions[sid] || '' }))
+        read('layout.html')
+            .pipe(hyperstream({ '#content': read(page) }))
             .pipe(res)
         ;
     }
-    else ecstatic(req, res);
 });
 server.listen(7000);
+
+function read (file) {
+    return fs.createReadStream(path.join(__dirname, 'static', file));
+}
