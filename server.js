35eb3c4829a07fd42b714b8e39828b767c2a62ed
James Halliday <mail@substack.net>
2015-01-26 00:39:10 -0800

    tcp-bind, post stub

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var url = require('url');
+var alloc = require('tcp-bind');
+
+var minimist = require('minimist');
+var argv = minimist(process.argv.slice(2), {
+    alias: { d: 'datadir', p: 'port', u: 'uid', g: 'gid' },
+    default: {
+        datadir: path.join(__dirname, 'sudoroom-data'),
+        port: require('is-root')() ? 80 : 8000
+    }
+});
+
+var fd = alloc(argv.port);
+if (argv.gid) process.setgid(argv.gid);
+if (argv.uid) process.setgid(argv.uid);
 
 var hyperstream = require('hyperstream');
 var ecstatic = require('ecstatic')({
     root: __dirname + '/static',
     gzip: true
 });
-
-var minimist = require('minimist');
-var argv = minimist(process.argv.slice(2), {
-    alias: { d: 'datadir' },
-    default: { datadir: path.join(__dirname, 'sudoroom-data') }
-});
+var body = require('body/any');
+var xtend = require('xtend');
 
 var level = require('level');
 var db = level(argv.datadir, { valueEncoding: 'json' });
@@ -39,8 +49,22 @@ var server = http.createServer(function (req, res) {
         ;
     }
 });
-server.listen(7000);
+server.listen({ fd: fd }, function () {
+    console.log('listening on :' + server.address().port);
+});
 
 function read (file) {
     return fs.createReadStream(path.join(__dirname, 'static', file));
+}
+
+function post (fn) {
+    return function (req, res, params) {
+        if (req.method !== 'POST') {
+            res.statusCode = 400;
+            res.end('not a POST\n');
+        }
+        else body(req, res, function (err, pvars) {
+            fn(req, res, xtend(pvars, params));
+        });
+    };
 }
