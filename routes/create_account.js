831c2d17bde980ada12dc95226f596e4fbcf03a4
James Halliday <mail@substack.net>
2015-02-05 23:30:03 -0800

    first account is a member

--- routes/create_account.js
+++ routes/create_account.js
@@ -2,9 +2,17 @@ var post = require('../lib/post.js');
 var crypto = require('crypto');
 var sublevel = require('subleveldown');
 var bytewise = require('bytewise');
+var through = require('through2');
 var retricon = require('retricon');
 
+var firstUser = false;
+
 module.exports = function (users, auth, blob) {
+    users.list().pipe(through.obj(
+        function (row) { firstUser = false },
+        function () { firstUser = true }
+    ));
+    
     return post(function (req, res, m) {
         var id = crypto.randomBytes(16).toString('hex');
         var img = retricon(id, { pixelSize: 15, tiles: 5 });
@@ -28,13 +36,15 @@ module.exports = function (users, auth, blob) {
                 name: m.params.name,
                 email: m.params.email,
                 fullName: m.params['full-name'],
-                member: false,
+                member: firstUser ? true : false,
                 visibility: m.params.visibility,
                 avatar: avatar,
                 created: date,
                 updated: date
             }
         };
+        firstUser = false;
+        
         users.create(id, opts, function (err) {
             if (err) return m.error(400, err);
             auth.login(res, { id: id, name: m.params.name }, onlogin);
