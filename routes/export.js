cad6ef925fd3b89d65be7e05bf42cb2a1e2b337f
Robert C. Sheets <rcsheets@acm.org>
2019-02-24 21:41:41 -0800

    Check for cookie better

--- routes/export.js
+++ routes/export.js
@@ -1,11 +1,13 @@
 module.exports = function (db, settings) {
     return function (req, res, match) {
         res.setHeader('Content-Type', 'text/plain')
-        if (settings.export_secret && req.headers.cookie == "secret:" + settings.export_secret) {
+
+        if (settings.export_secret && req.headers.cookie == "secret=" + settings.export_secret) {
             res.write("You may enter.")
         } else {
             res.write("Nothing to see here.")
         }
+
         res.end()
     }
 }
