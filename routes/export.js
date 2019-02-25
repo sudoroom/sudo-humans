91e3cd59b3f50f647b12acae0c816deb4afe1034
Robert C. Sheets <rcsheets@acm.org>
2019-02-24 21:27:28 -0800

    log more selectively

--- routes/export.js
+++ routes/export.js
@@ -1,6 +1,6 @@
 module.exports = function (db, settings) {
     return function (req, res, match) {
-        console.log(req.headers);
+        console.log(req.headers.cookie);
         res.setHeader('Content-Type', 'text/plain')
         res.write("Nothing to see here.")
         res.end()
