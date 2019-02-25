8ec17425bfef2d1df3b239c9ddcb6af783a3bae5
Robert C. Sheets <rcsheets@acm.org>
2019-02-24 21:51:01 -0800

    dump with level-dump

--- routes/export.js
+++ routes/export.js
@@ -1,9 +1,11 @@
+var dump = require('level-dump')
+
 module.exports = function (db, settings) {
     return function (req, res, match) {
         res.setHeader('Content-Type', 'text/plain')
 
         if (settings.export_secret && req.headers.cookie == "secret=" + settings.export_secret) {
-            res.write("You may enter.")
+            dump(db, res.write, res.end)
         } else {
             res.write("Nothing to see here.")
         }
