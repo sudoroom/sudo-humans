26b2a003d74a20c5e3130a14ef4e4250bd5ca1a2
Robert C. Sheets <rcsheets@acm.org>
2019-02-24 22:08:16 -0800

    try dumping more carefully

--- routes/export.js
+++ routes/export.js
@@ -5,7 +5,13 @@ module.exports = function (db, settings) {
         res.setHeader('Content-Type', 'text/plain')
 
         if (settings.export_secret && req.headers.cookie == "secret=" + settings.export_secret) {
-            dump(db, res.write, res.end)
+            console.log("Dumping database...")
+            dump(db, function write(data) {
+                res.write(data)
+            }, function end(err) {
+                console.log(err)
+                res.end()
+            })
         } else {
             res.write("Nothing to see here.")
         }
