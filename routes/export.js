05c66e92d0e41683e62b732d524d79238898e774
Robert C. Sheets <rcsheets@acm.org>
2019-02-24 21:13:33 -0800

    Make the export route send a response

--- routes/export.js
+++ routes/export.js
@@ -1,5 +1,8 @@
 module.exports = function (db, settings) {
     return function (req, res, match) {
         console.log(req.headers);
+        res.setHeader('Content-Type', 'text/plain')
+        res.write("Nothing to see here.")
+        res.end()
     }
 }
