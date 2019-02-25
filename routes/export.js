fe353a6f2de469bd77c58bffa13153a82b7149d7
Robert C. Sheets <rcsheets@acm.org>
2019-02-24 21:33:45 -0800

    Check for secret cookie

--- routes/export.js
+++ routes/export.js
@@ -1,8 +1,11 @@
 module.exports = function (db, settings) {
     return function (req, res, match) {
-        console.log(req.headers.cookie);
         res.setHeader('Content-Type', 'text/plain')
-        res.write("Nothing to see here.")
+        if (settings.export_secret && req.headers.cookie == "secret:" + settings.export_secret) {
+            res.write("You may enter.")
+        } else {
+            res.write("Nothing to see here.")
+        }
         res.end()
     }
 }
