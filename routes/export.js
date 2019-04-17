222998fb8bf293afaf5cd8d77584c2104475a53a
Robert C. Sheets <rcsheets@acm.org>
2019-04-16 19:30:44 -0700

    support dumping all three leveldb databases

--- routes/export.js
+++ routes/export.js
@@ -4,9 +4,25 @@ var util = require('util')
 module.exports = function (datalevel, ixdb, sessions, settings) {
     return function (req, res, match) {
         res.setHeader('Content-Type', 'text/plain')
-        console.log("export request received for " + req.path)
+        console.log("export request received for " + match.params.database)
 
         if (settings.export_secret && req.headers.cookie == "secret=" + settings.export_secret) {
+            switch (match.params.database) {
+                case "datalevel":
+                    db = datalevel;
+                    break;
+                case "ixdb":
+                    db = ixdb;
+                    break;
+                case "sessions":
+                    db = sessions;
+                    break;
+                default:
+                    res.writeHead(404, { "Content-Type": "text/plain" });
+                    res.end("Not Found");
+                    return;
+            }
+
             console.log("Dumping database...")
             response = ""
             dump.allEntries(db, function write(data) {
